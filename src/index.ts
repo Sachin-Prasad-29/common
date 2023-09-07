c7b529d0fa35c3acb3ba1cbd84bb45c4d18ef1f2
Sachin <skpkorba9009@gmail.com>
2023-09-07 22:23:54 +0530

    addition config

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+const color: Color = {
+    red: 20,
+    blue: 240,
+    green: 255
+};
+
+console.log(color);
+
+export default color;
